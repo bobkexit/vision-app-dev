42c06197df85021141e958f85173eed138a397b6
Bob Kexit <bobkexit@gmail.com>
2017-12-21 19:19:56 +0300

    setup UI

--- vision-app-dev/Controllers/CameraVC.swift
+++ vision-app-dev/Controllers/CameraVC.swift
@@ -7,13 +7,81 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CameraVC: UIViewController {
+class CameraVC: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    // Outlets
+    @IBOutlet weak var captureImgView: UIImageView!
+    @IBOutlet weak var flashBtn: UIButton!
+    @IBOutlet weak var identificationLbl: UILabel!
+    @IBOutlet weak var confidenceLbl: UILabel!
+    @IBOutlet weak var cameraView: UIView!
+    
+    // Variables
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var photoData: Data?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
+        cameraView.addGestureRecognizer(tap)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: .video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput!)
+            }
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            previewLayer.connection?.videoOrientation = .portrait
+            
+            cameraView.layer.addSublayer(previewLayer!)
+            captureSession.startRunning()
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        cameraOutput.capturePhoto(with: settings, delegate: self)
     }
 
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+            return
+        }
+        photoData = photo.fileDataRepresentation()
+        if let photoData = photoData {
+             captureImgView.image = UIImage(data: photoData)
+        }
+    }
 }
 
---
+++ vision-app-dev/Views/RoundedShadowButton.swift
@@ -0,0 +1,30 @@
+//
+//  RoundedShadowButton.swift
+//  vision-app-dev
+//
+//  Created by Николай Маторин on 21.12.2017.
+//  Copyright © 2017 Николай Маторин. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedShadowButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
+    }
+
+}
